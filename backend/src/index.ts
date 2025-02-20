8f90c80ba9e9fdf2ca5c5370aa99b389df56de5a
yuval <yuvalc11499@gmail.com>
2025-02-20 14:29:36 +0200

    Setup DB connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
 import { config } from "./config";
+import db from "./db";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await db.execute("SELECT * FROM users");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database query error");
+  }
 });
 
 app.listen(config.PORT, () => {
